a19807f59383aff1cba7cd93accbd30c2e6ebff9
Hass <hassmroueh@gmail.com>
2024-01-20 02:39:11 -0500

    html complete

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "What does it means to have Sabr?";
 document.querySelector("body").appendChild(h2);
